52f9d3b8b33286c5fcffc62c9dc0a2fd6443cdac
李景枫 <595208882@qq.com>
2016-07-07 23:45:20 +0800

    no commit message

--- src/test/java/cn/ms/sequence/SequenceTest.java
+++ src/test/java/cn/ms/sequence/SequenceTest.java
@@ -3,18 +3,7 @@ package cn.ms.sequence;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Test;
-
 public class SequenceTest {
-	
-	@Test
-	public void name() {
-			Sequence sequence = new Sequence(0, 0);
-			for (int i = 0; i < 1000; i++) {
-				long id = sequence.nextId();
-				System.out.println(id);
-			}
-	}
 	
 	public static void main(String[] args) {
 		Set<Long> set = new HashSet<Long>();
