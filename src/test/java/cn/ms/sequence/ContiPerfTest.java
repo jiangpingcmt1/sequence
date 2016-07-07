5d21e37c6dba1a56a0549ec90731c83014119ec3
李景枫 <595208882@qq.com>
2016-07-07 23:57:11 +0800

    no commit message

--- src/test/java/cn/ms/sequence/ContiPerfTest.java
+++ src/test/java/cn/ms/sequence/ContiPerfTest.java
@@ -1,7 +1,6 @@
 package cn.ms.sequence;
 
 import org.databene.contiperf.PerfTest;
-import org.databene.contiperf.Required;
 import org.databene.contiperf.junit.ContiPerfRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,10 +10,12 @@ public class ContiPerfTest {
 	@Rule
 	public ContiPerfRule i = new ContiPerfRule();
 
+	Sequence sequence = new Sequence(0, 0);
+	
 	@Test
 	@PerfTest(invocations = 1000000, threads = 16)
 	public void test1() throws Exception {
-		
+		sequence.nextId();
 	}
 
 }
