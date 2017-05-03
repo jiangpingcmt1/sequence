7456121252892ea22ad9bb6fa0829623046febc4
李景枫 <595208882@qq.com>
2017-05-03 11:55:36 +0800

    no commit message

--- src/main/java/cn/ms/sequence/Sequence.java
+++ src/main/java/cn/ms/sequence/Sequence.java
@@ -47,10 +47,23 @@ public class Sequence {
 	 */
 	public synchronized long nextId() {
 		long timestamp = timeGen();
-		if (timestamp < lastTimestamp) {
-			throw new RuntimeException(String.format(
-					"Clock moved backwards. Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+		if (timestamp < lastTimestamp) {//闰秒
+			long offset = lastTimestamp - timestamp;
+			if (offset <= 5) {
+				try {
+					wait(offset << 1);
+					timestamp = timeGen();
+					if (timestamp < lastTimestamp) {
+						throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", offset)); 
+					}
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			} else {
+				throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", offset));
+			}
 		}
+		
 		if (lastTimestamp == timestamp) {
 			sequence = (sequence + 1) & sequenceMask;
 			if (sequence == 0) {
@@ -62,8 +75,7 @@ public class Sequence {
 
 		lastTimestamp = timestamp;
 
-		return ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift)
-				| (workerId << workerIdShift) | sequence;
+		return ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift) | (workerId << workerIdShift) | sequence;
 	}
 
 	protected long tilNextMillis(long lastTimestamp) {
